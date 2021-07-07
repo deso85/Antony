908f4f894258b280633fec7621a6fd564f4958b6
deso85 <github@cizero.de>
2021-07-07 19:18:24 +0200

    v2.6.1

--- src/main/java/bot/antony/events/EggReactionNotification.java
+++ src/main/java/bot/antony/events/EggReactionNotification.java
@@ -72,28 +72,38 @@ public class EggReactionNotification extends ListenerAdapter {
 		TextChannel roles;
 		TextChannel serverQuestions;
 		TextChannel serverRules;
+		TextChannel memberIntroduction;
 		if(Antony.isProdStage()) {
 			roles = guild.getTextChannelById(745657357962313749L);
 			serverQuestions = guild.getTextChannelById(692317004727844934L);
 			serverRules = guild.getTextChannelById(789417087382192148L);
+			memberIntroduction = guild.getTextChannelById(543512466739691520L);
 		} else {
 			roles = guild.getTextChannelById(778960515895918628L);
 			serverQuestions = guild.getTextChannelById(778960515895918630L);
 			serverRules = guild.getTextChannelById(778960515895918626L);
+			memberIntroduction = guild.getTextChannelById(778960515895918631L);
 		}
 		ArrayList<String> messages = new ArrayList<String>();
 		
 		messages.add("Willkommen " + member.getAsMention() + "!\n"
 				+ "Du bist nun freigeschaltet und siehst schon mehr Kanäle. "
-				+ "Falls du noch weitere tolle Bereiche z.B. über andere Tiere und Pflanzen oder die Haltungsberichte entdecken möchtest, sieh dir in der " + roles.getAsMention() + " die Module an!"
-				+ " Bei Fragen melde dich gerne bei einem Mod oder in " + serverQuestions.getAsMention() + ".\nViel Spaß!");
+				+ "Falls du noch weitere tolle Bereiche z.B. über andere Tiere und Pflanzen oder die Haltungsberichte entdecken möchtest, sieh dir in der " + roles.getAsMention() + " die Module an! "
+				+ "Bei Fragen melde dich gerne bei einem Mod oder in " + serverQuestions.getAsMention() + ".\nViel Spaß!");
 
 		messages.add("Willkommen " + member.getAsMention() + "!\n"
-				+ "Du bist nun freigeschaltet und kannst auch in anderen Bereichen schreiben und dich aktiv einbringen."
-				+ " Schau dir gerne noch den Kanal " + serverRules.getAsMention() + " an, bevor du richtig los legst."
-				+ " Weitere Server-Inhalte kannst du in dem Kanal " + roles.getAsMention() + " freischalten."
-				+ " Bei Fragen kannst du dich jeder Zeit an einen Mod wenden oder dich in " + serverQuestions.getAsMention() + " melden."
+				+ "Du bist nun freigeschaltet und kannst auch in anderen Bereichen schreiben und dich aktiv einbringen. "
+				+ "Schau dir gerne noch den Kanal " + serverRules.getAsMention() + " an, bevor du richtig los legst. "
+				+ "Weitere Server-Inhalte kannst du in dem Kanal " + roles.getAsMention() + " freischalten. "
+				+ "Bei Fragen kannst du dich jeder Zeit an einen Mod wenden oder dich in " + serverQuestions.getAsMention() + " melden."
 				+ "\nUnd nun viel Spaß bei uns!");
+		
+		messages.add("Willkommen " + member.getAsMention() + "!\n"
+				+ "Du bist nun freigeschaltet und siehst schon mehr Kanäle. "
+				+ "Weitere Themen, die dich interessieren könnten, kannst du in " + roles.getAsMention() + " als Module zuschalten! "
+				+ "Nützliche Informationen findest du in " + serverRules.getAsMention() + ". "
+				+ "In " + memberIntroduction.getAsMention() + " kannst du dich, wenn du möchtest, gerne kurz vorstellen."
+				+ "\nViel Spaß auf AAM!");
 		
 		return messages.get((int) (System.currentTimeMillis() % messages.size()));
 	}
